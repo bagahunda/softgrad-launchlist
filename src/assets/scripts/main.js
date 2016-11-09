6ffc0bea18ecf97719917b15a7427b3e6b486eec
Dmitriy Pochodnya <dmitriy.troy@gmail.com>
2016-11-09 17:23:47 +0300

    wip

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('Its worked. Im from mainjs');
+(function ($) {
+
+  $(document).ready(function() {
+
+    var totalPoints = parseInt($('.launchlist li').length);
+    var checkedPoints = 0;
+    var ready = 0;
+
+    $('.toggle__input').on('change', function() {
+      if ($(this).is(':checked')) {
+        $(this).parent().addClass('toggle--is-active');
+        checkedPoints++;
+        ready = Math.round(checkedPoints*100/totalPoints);
+        if (ready === 100) {
+          $('.status__comand').html('ПОЕХАЛИ!');
+        }
+        $('.status__ready').html(ready + '%');
+        return false
+      } else {
+        $(this).parent().removeClass('toggle--is-active');
+        checkedPoints--;
+        ready = Math.round(checkedPoints*100/totalPoints)
+        $('.status__ready').html(ready + '%');
+        return false
+      }
+    })
+
+  });
+
+})(jQuery);
